0194217068bb226b9272296cc9e59536cabacefb
Cody Botte <cbotte21@gmail.com>
2022-11-30 12:43:53 -0600

    Moved mongo uri to enviroment variables

--- datastore/connection.go
+++ datastore/connection.go
@@ -5,11 +5,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"os"
 )
 
-const uri string = "mongodb+srv://root:Asdfasdf1@cluster0.btzdz.mongodb.net/?retryWrites=true&w=majority" //TODO: Pull from env variables
-
 func GetMongoClient() (*mongo.Client, error) {
+	uri := os.Getenv("auth_mongo_uri")
+
 	//connect
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 
